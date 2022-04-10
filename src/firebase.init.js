98c0c18ef20922b408809a4eeb3e11802202f422
web-altaf-1 <web.altaf.1@gmail.com>
2022-04-10 22:05:21 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAztNiP8mlzrHGUBq0R6nVovtR0Rn12AOM",
+  authDomain: "router-firebase-integrat-e3f53.firebaseapp.com",
+  projectId: "router-firebase-integrat-e3f53",
+  storageBucket: "router-firebase-integrat-e3f53.appspot.com",
+  messagingSenderId: "892507168869",
+  appId: "1:892507168869:web:c5549da7107dceb088268c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app ;
