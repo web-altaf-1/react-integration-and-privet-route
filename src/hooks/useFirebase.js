0884d4e267c5aea0129678c4d9063cd95961e3c9
web-altaf-1 <web.altaf.1@gmail.com>
2022-04-11 02:03:22 +0600

    added on auth state change to perchist auth

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,13 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import app from "../firebase.init";
+
+const auth = getAuth(app);
+
+const GoogleProvider = new GoogleAuthProvider();
 
 const useFirebase = () =>{
     const [user,setUser] = useState({});
     
     const signInWithGoogle =()=>{
-        console.log('Sign in soon !! ');
+        signInWithPopup(auth,GoogleProvider)
+        .then(result => {
+            const user = result.user ;
+            setUser(user)
+            console.log(user);
+        })
     }
 
-    return {user, signInWithGoogle };
+    const handleSignOut = () =>{
+        signOut(auth)
+        .then(()=>{})
+    }
+
+    useEffect(()=>{
+        onAuthStateChanged(auth , user =>{
+            setUser(user);
+        })
+    },[])
+
+    return {user, signInWithGoogle , handleSignOut };
 }
 
 export default useFirebase;
