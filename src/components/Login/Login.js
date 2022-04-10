908210e124796188199e3d6a406b8e111ef885a2
web-altaf-1 <web.altaf.1@gmail.com>
2022-04-10 23:23:07 +0600

    firebase component add on custom hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
-
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h3>Please login</h3>
             <div style={{margin:'20px'}}>
-                <button>Sign in with Google</button>
+                <button onClick={signInWithGoogle}>Sign in with Google</button>
             </div>
             <form >
-                <input type="email" name="" id="" placeholder='Your Email' />
+                <input type="email" name="email"  placeholder='Your Email' />
                 <br />
-                <input type="password" name="" id="" placeholder='Password' />
+                <input type="password" name="" placeholder='Password' />
                 <br />
                 <input type="submit" value="Login" />
             </form>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,16 +1,13 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 const useFirebase = () =>{
     const [user,setUser] = useState({});
-    useEffect(()=>{
-
-    }, []);
-
+    
     const signInWithGoogle =()=>{
         console.log('Sign in soon !! ');
     }
 
-    return {user,setUser};
+    return {user, signInWithGoogle };
 }
 
 export default useFirebase;
